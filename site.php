6c4c7b930b6e2f0fac03f11fdc2d53a521dc53b7
KarimahSwan <71859529+KarimahSwan@users.noreply.github.com>
2022-01-09 11:04:39 -0500

    Add files via upload

---
+++ site.php
@@ -0,0 +1,29 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>
+
+        </title>
+    </head>
+    <body>
+        <?php
+            //variable
+            $name = "Adam";
+            $age =21;
+            $welcome = "Welcome to Kimm Aesthetics";
+            //print statements
+            echo("hello");
+            echo "<h1>Kimm Aesthetics</h1>";
+            echo "<hr>";
+            echo "<p>This is Kimm's Aesthetics</p> $welcome";
+        ?>
+    </body>
+    <footer>
+        <?php
+            echo "<p>Cofounded by $name who is $age yearsold.";
+            $name = "Kimm";
+            $age = 20;
+            echo "<p>Cofounded by $name who is $age yearsold.";
+            ?>
+    </footer>
+</html>
